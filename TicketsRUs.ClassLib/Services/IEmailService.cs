506af0e9abe2f3bf74ae4862a5380c8d5b5fea65
Garion Cox <garion.cox@students.snow.edu>
2024-02-14 15:23:44 -0700

    Updated IEmailService

--- TicketsRUs.ClassLib/Services/IEmailService.cs
+++ TicketsRUs.ClassLib/Services/IEmailService.cs
@@ -8,10 +8,6 @@ namespace TicketsRUs.ClassLib.Services
 {
     public interface  IEmailService
     {
-<<<<<<< HEAD
         Task SendEmailAsync(string receiverEmail, string identifier);
-=======
-        Task SendEmailAsync( string receiverEmail, string identifier);
->>>>>>> c55a7690086f51676d1cbbddb0fb82dea60db62a
     }
 }
