3ad289811e8240feea766d1cca8c5e022a817054
luris.solis <luris.solis@students.snoe.edu>
2024-02-16 12:22:12 -0700

    change localdatabases

--- TicketsRUs.ClassLib/Constants.cs
+++ TicketsRUs.ClassLib/Constants.cs
@@ -9,13 +9,5 @@ namespace TicketsRUs.ClassLib;
 public static class Constants
 {
     public const string DataBaseFileName = "Local.db3";
-    public const SQLite.SQLiteOpenFlags Flags =
-    // open the database in read/write mode
-        SQLite.SQLiteOpenFlags.ReadWrite |
-    // create the database if it doesn't exist
-        SQLite.SQLiteOpenFlags.Create |
-    // enable multi-threaded database access
-        SQLite.SQLiteOpenFlags.SharedCache;
-
-    public static string DatabasePath => Path.Combine(Directory.GetCurrentDirectory(), DataBaseFileName);
+    public static string DatabasePath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData));
 }
