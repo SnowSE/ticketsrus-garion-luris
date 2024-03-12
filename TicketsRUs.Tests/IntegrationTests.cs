ed3e93c4239450be32c030c6ef35d9aa6683140d
garioncox <garionlcox@gmail.com>
2024-03-12 13:02:33 -0600

    Checkpoint

---
+++ TicketsRUs.Tests/IntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+
+using TicketsRUs.WebApp;
+using TicketsRUs.ClassLib.Data;
+using TicketsRUs.Maui.Components;
+using TicketsRUs.ClassLib.Services;
+using TicketsRUs.Maui.Services;
+
+namespace TicketsRUs.Tests;
+
+public class IntegrationTests : IClassFixture<TestFactory>
+{
+    public HttpClient client { get; set; }
+    public IntegrationTests(TestFactory Factory)
+    {
+        client = Factory.CreateDefaultClient();
+    }
+
+    [Fact]
+    public void CanPassATest()
+    {
+        Assert.Equal(1, 1);
+    }
+}
